a44b3b33718091b2f9b6d5d362ec46bf707d728c
Yi Huang <u6039034@anu.edu.au>
2018-03-07 11:35:03 +1100

    Update

--- code/embedding_train.py
+++ code/embedding_train.py
@@ -1,16 +1,34 @@
 import gensim
 import logging
 import mysql.connector
+import os
 from prepros import get_words
 
+fname = os.path.join(os.pardir, "data", "mymodel")
 
+start = 0
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-cnx = mysql.connector.connect(host='localhost',
-                              user='root',
-                              password='yfwrshgrm',
-                              db='stackoverflow')
-cursor = cnx.cursor()
-query = "SELECT Id, Body FROM Posts WHERE Score >= 0 AND Id >= {} AND Id < {}".format(start, start+200000)
-cursor.execute(query)
-for current_id, row in cursor.fetchall():
+
+class MySentences(object):
+
+    def __iter__(self):
+        current_id = 0
+        try:
+            cnx = mysql.connector.connect(host='localhost',
+                                          user='root',
+                                          password='yfwrshgrm',
+                                          db='stackoverflow')
+            cursor = cnx.cursor()
+            query = "SELECT Id, Body FROM Posts WHERE Score >= 0"
+            cursor.execute(query)
+            for current_id, row in cursor.fetchall():
+                words_list = get_words(row)
+                for words in words_list:
+                    yield words
+        finally:
+            print("current_id: {}".format(current_id))
+
+sentences = MySentences()
+model = gensim.models.Word2Vec(sentences, min_count=10, size=200, workers=8)
+model.save(fname)
--- weekly summary/summary.md
+++ weekly summary/summary.md
@@ -1,5 +1,5 @@
-| Posts    | Tags  | Pairs |
-| -------- | ----- | ----- |
-| 38485046 | 50812 | 64863 |
+| Posts    | Tags  | Pairs | Id_MAX   |
+| -------- | ----- | ----- | -------- |
+| 38485046 | 50812 | 64863 | 47615428 |
 
 similar tech pairs after deleting synonyms: 64687
