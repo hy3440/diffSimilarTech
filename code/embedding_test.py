3cc7a78cd87c90f05c9c85d749fa951a8d998014
Yi Huang <u6039034@anu.edu.au>
2018-03-26 21:28:34 +1100

    Update

--- code/embedding_test.py
+++ code/embedding_test.py
@@ -3,6 +3,6 @@ import logging
 import os
 
 
-fname = os.path.join(os.pardir, "data", "mymodel")
+fname = os.path.join(os.pardir, "data", "mymodel30000000")
 model = gensim.models.Word2Vec.load(fname)
 print(model.wv.most_similar(positive=['performance']))
